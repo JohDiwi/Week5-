504a99565cb04a55c3222e29e5970448d51988e0
JohDiwi <162934025+JohDiwi@users.noreply.github.com>
2024-05-11 14:30:54 +0300

    Add files via upload

---
+++ file_handling_assignment.py
@@ -0,0 +1,15 @@
+with open("my_file.txt", "r") as file:
+    content=file.read()
+print(content)
+
+with open("my_file.txt", "a") as file:
+    file.write("Hower I am still in week 9\n"+"I shuld pull my socks to fininsh in time\n")
+    file.write("Though its a bit challenging")
+
+try:
+    with open("Exact.py", "r") as file: 
+        message=file.read()
+        print(message)
+except (FileNotFoundError, PermissionError): 
+    print("Error!, Cant open this file") 
+
